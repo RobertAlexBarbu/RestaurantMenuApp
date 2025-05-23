39c38a559958f69195891380c20c5303d3753f81
Robert Alex Barbu <robertalexbarbu@gmail.com>
2025-05-23 03:30:17 +0300

    some work on menu Details

--- WebAPI/WebAPI.Domain/Entities/Menu/MenuDetails.cs
+++ WebAPI/WebAPI.Domain/Entities/Menu/MenuDetails.cs
@@ -7,4 +7,34 @@ public class MenuDetails
     public Menu Menu { get; set; }
     public int UserId { get; set; }
     public User User { get; set; }
+    
+    // // WiFi Information
+    // public string WifiNetworkName { get; set; } = string.Empty;
+    // public string WifiPassword { get; set; } = string.Empty;
+    // public bool HasFreeWifi { get; set; }
+    //
+    // // Contact Information
+    // public string PhoneNumber { get; set; } = string.Empty;
+    // public string Email { get; set; } = string.Empty;
+    // public string WebsiteUrl { get; set; } = string.Empty;
+    // public string FacebookPage { get; set; } = string.Empty;
+    // public string InstagramHandle { get; set; } = string.Empty;
+    //
+    // // Opening Hours
+    // public string MondayHours { get; set; } = string.Empty;
+    // public string TuesdayHours { get; set; } = string.Empty;
+    // public string WednesdayHours { get; set; } = string.Empty;
+    // public string ThursdayHours { get; set; } = string.Empty;
+    // public string FridayHours { get; set; } = string.Empty;
+    // public string SaturdayHours { get; set; } = string.Empty;
+    // public string SundayHours { get; set; } = string.Empty;
+    // public string SpecialHoursNote { get; set; } = string.Empty;
+    //
+    // // Address Information
+    // public string StreetAddress { get; set; } = string.Empty;
+    // public string City { get; set; } = string.Empty;
+    // public string StateProvince { get; set; } = string.Empty;
+    // public string PostalCode { get; set; } = string.Empty;
+    // public string Country { get; set; } = string.Empty;
+    // public string GoogleMapsLink { get; set; } = string.Empty;
 }
--- WebApp/src/app/features/menu-feature/components/details-page/details-page.component.ts
+++ WebApp/src/app/features/menu-feature/components/details-page/details-page.component.ts
@@ -5,7 +5,7 @@ import {MatMenu, MatMenuItem} from "@angular/material/menu";
 import {AsyncPipe, NgTemplateOutlet} from "@angular/common";
 import {ToolbarComponent} from "../../../../shared/components/toolbar/toolbar.component";
 import {CardComponent} from "../../../../shared/components/card/card.component";
-import {MatSlideToggle} from "@angular/material/slide-toggle";
+import {MatSlideToggle, MatSlideToggleChange} from "@angular/material/slide-toggle";
 import {pageLoadAnimation} from "../../../../app.animations";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
@@ -45,12 +45,59 @@ export class DetailsPageComponent {
     })
     disabledWifiSave = signal(true)
     updateWifiLoading = signal(false)
+    saveWifiDetails() {
+        if (this.wifiForm.invalid) {
+            return;
+        }
+    }
 
+    
+    contactForm = new FormGroup({
+        phoneNumber: new FormControl(''),
+        email: new FormControl(''),
+        facebookUrl: new FormControl(''),
+        tiktokUrl: new FormControl(''),
+    })
+    disabledContactSave = signal(true)
+    updateContactLoading = signal(false)
+    saveContactDetails() {
+        if (this.wifiForm.invalid) {
+            return;
+        }
+    }
+    
+    addressForm = new FormGroup({
+        street: new FormControl(''),
+        city: new FormControl(''),
+        state: new FormControl(''),
+        zipCode: new FormControl(''),
+    })
+    disabledAddressSave = signal(true)
+    updateAddressLoading = signal(false)
+    saveAddressDetails() {
+        if (this.wifiForm.invalid) {
+            return;
+        }
+    }
+    
     constructor() {
         this.wifiForm.valueChanges.subscribe({
             next: (data) => {
                 this.disabledWifiSave.set(false);
             }
         })
+        this.contactForm.valueChanges.subscribe({
+            next: (data) => {
+                this.disabledContactSave.set(false);
+            }
+        })
+        this.addressForm.valueChanges.subscribe({
+            next: (data) => {
+                this.disabledAddressSave.set(false);
+            }
+        })
+        
     }
+    
+
 }
