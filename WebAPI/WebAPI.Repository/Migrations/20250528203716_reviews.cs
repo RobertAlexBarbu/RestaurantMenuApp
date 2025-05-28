29d0955c478a3e0a1e59e749d8b14bac542d54a1
Robert Alex Barbu <robertalexbarbu@gmail.com>
2025-05-28 23:46:05 +0300

    Finish reviews feature

---
+++ WebAPI/WebAPI.Repository/Migrations/20250528203716_reviews.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebAPI.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class reviews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MenuReviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Rating = table.Column<string>(type: "text", nullable: false),
+                    Message = table.Column<string>(type: "text", nullable: false),
+                    MenuId = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuReviews", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MenuReviews_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MenuReviews_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuReviews_MenuId",
+                table: "MenuReviews",
+                column: "MenuId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuReviews_UserId",
+                table: "MenuReviews",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MenuReviews");
+        }
+    }
+}
---
+++ WebAPI/WebAPI.Repository/Migrations/20250528204055_fix reviews.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebAPI.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixreviews : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuReviews_Users_UserId",
+                table: "MenuReviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MenuReviews_UserId",
+                table: "MenuReviews");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "MenuReviews");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "MenuReviews",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuReviews_UserId",
+                table: "MenuReviews",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuReviews_Users_UserId",
+                table: "MenuReviews",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
